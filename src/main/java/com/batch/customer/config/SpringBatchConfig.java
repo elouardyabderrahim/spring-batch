c463317acd22b869b6e06ca5a37884a5a3f10713
unknown <elouardy.abderrahim07@gmail.com>
2024-07-09 16:52:40 +0100

    create the reader
    customerReadermethod to set the line mapper we creted the lineMapper() methode that mappes each line to object

--- src/main/java/com/batch/customer/config/SpringBatchConfig.java
+++ src/main/java/com/batch/customer/config/SpringBatchConfig.java
@@ -1,2 +1,52 @@
-package com.batch.customer.config;public class SpringBatchConfig {
+package com.batch.customer.config;
+
+
+import com.batch.customer.entity.Customer;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+@Configuration
+@EnableBatchProcessing
+public class SpringBatchConfig {
+
+// Create the reader
+    @Bean
+    public FlatFileItemReader<Customer> customerReader(){
+
+        FlatFileItemReader<Customer> itemReader= new FlatFileItemReader<>();
+        Resource resource=new FileSystemResource("customers.csv");
+        itemReader.setResource(resource);
+        itemReader.setName("csv_customer_reader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+
+
+        return itemReader;
+    }
+
+    private LineMapper<Customer> lineMapper() {
+
+        DefaultLineMapper<Customer> lineMapper= new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer=new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);// allow some empty columns
+        lineTokenizer.setNames("id", "firstName", "lastName", "email", "gender", "contactNo", "country", "dob");
+
+        // convert data to customer bean object
+        BeanWrapperFieldSetMapper<Customer> fieldSetMapper=new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Customer.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+    }
 }
+
